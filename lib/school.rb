1d6cf703ce9d91a857b71168a42fefecb3ca2f66
jgumerove <josh.gumerove@gmail.com>
2021-02-18 14:16:15 -0500

    commit tree 0951d812aa471c9ccf6a209f447c3edc73830f5b

--- lib/school.rb
+++ lib/school.rb
@@ -1,34 +1,32 @@
-# code here!
 require 'pry'
 
 class School
-  attr_accessor :name, :roster, :grade
-  def initialize(name)
-    @name = name
+
+  def initialize(roster)
     @roster = {}
   end
 
-  def add_student(name, grade)
-      if !@roster.key?(grade)
-        @roster[grade] = []
-        @roster[grade] << name
-      else
-        @roster[grade] << name
-      end
+  def roster
+    @roster
+  end
 
+  def add_student(name, grade)   
+  if @roster[grade]
+    @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+     end
+   end
+   
+   def grade(grade)
+    @roster[grade]
+  end
+  def sort 
+    @roster.each_value do |value|
+      value.sort!
     end
+  end
 
-    def grade(grade)
-      if @roster.key?(grade)
-        @roster[grade]
-      end
-    end
-
-    def sort
-      @roster.each_value do |key|
-        key.sort!
-      end
-      @roster
-
-    end
 end
+
